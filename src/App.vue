b4215e815579f7bd00c73fbb856e6bb48d3caaae
Gabi Farias <gabijk1234row@gmail.com>
2024-02-11 14:30:06 -0400

    [NEW] including i18n

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <img class="hidden lg:block absolute top-0 right-0 w-1/6 px-4 mt-4 mr-4" src="../public/images/logo.png" />
     <div class="pb-24">
       <h1 class="flex justify-center font-bold text-6xl">
-        Analisando dados da nossa empresa
+        {{ $t('tittle') }}
       </h1>
       <p class="flex justify-center font-medium text-3xl"> 
-        Vamos analisar dados do período de um mês (30 dias) da nossa empresa.
+        {{ $t('subtittle') }}
       </p>
     </div>
     <ResultsView />
--- src/composable/ResultsView.vue
+++ src/composable/ResultsView.vue
@@ -2,7 +2,7 @@
   <div class="h-96 w-auto bg-light-red border-4 border-solid border-secundary rounded-md flex justify-center">
     <div class="flex flex-col items-center p-2">
       <h1 class="font-bold text-3xl flex justify-center pt-4 mb-16">
-        O que deseja ver?
+        {{ $t('view.to_see') }}
       </h1>
       <div class="flex flex-row justify-center items-center p-2">
         <LowestRevenueButton :isActive="activeButton === 'low'" @click="handleButtonClick('low')" :resultMessage="resultMessages.low" />
